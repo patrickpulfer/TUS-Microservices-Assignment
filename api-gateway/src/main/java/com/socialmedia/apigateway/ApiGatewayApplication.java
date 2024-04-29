5614c1fe23e2807cea9999a3f0764b213441bf21
patrickpulfer <patrick.pulfer1@gmail.com>
2024-04-29 19:01:12 +0100

    Add: Cloud Config for API Gateway

--- api-gateway/src/main/java/com/socialmedia/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/socialmedia/apigateway/ApiGatewayApplication.java
@@ -2,14 +2,14 @@ package com.socialmedia.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.net.http.HttpClient;
 
-
+@EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -17,10 +17,4 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
-//    @Bean
-//    public HttpClient httpClient() {
-//        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
-//    }
-
 }
--- user/src/main/java/com/socialmedia/user/controller/UserController.java
+++ user/src/main/java/com/socialmedia/user/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/v1/api/")
 public class UserController
 {
     @Autowired
